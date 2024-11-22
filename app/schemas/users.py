1b415bc03b23ddc91e11452c920de98708343c21
andrey <palanelarty@gmail.com>
2024-11-22 16:10:29 +0300

    add schemas for users

---
+++ app/schemas/users.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, EmailStr
+
+
+class UserRequestAdd(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserAdd(BaseModel):
+    email: EmailStr
+    hashed_password: str
