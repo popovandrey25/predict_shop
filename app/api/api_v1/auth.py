2b89210f526c4f414aad731f7f44457ee8826fce
andrey <palanelarty@gmail.com>
2024-11-24 13:19:25 +0300

    add simple auth

--- app/api/api_v1/auth.py
+++ app/api/api_v1/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Response, Depends
 
-from api.api_v1.dependencies import DBDep
+from api.api_v1.dependencies import DBDep, UserIdDep
 from schemas.user import UserRequestAdd, UserAdd
 from services.auth import AuthService
 
@@ -21,30 +21,30 @@ async def register_user(data: UserRequestAdd, db: DBDep):
 
     return {"status": "OK"}
 
-#
-# @router.post("/login")
-# async def login_user(
-#     data: UserRequestAdd,
-#     response: Response,
-#     db: DBDep,
-# ):
-#     user = await db.users.get_user_with_hashed_password(email=data.email)
-#     if not user:
-#         raise HTTPException(status_code=401, detail="Пользователь с таким email не зарегистрирован")
-#     if not AuthService().verify_password(data.password, user.hashed_password):
-#         raise HTTPException(status_code=401, detail="Пароль неверный")
-#     access_token = AuthService().create_access_token({"user_id": user.id})
-#     response.set_cookie("access_token", access_token)
-#     return {"access_token": access_token}
-#
-#
-# @router.get("/me")
-# async def get_me(user_id: UserIdDep, db: DBDep):
-#     user = await db.users.get_one_or_none(id=user_id)
-#     return user
-#
-#
-# @router.post("/logout")
-# async def logout_user(response: Response):
-#     response.delete_cookie('access_token')
-#     return {"status": "OK"}
+
+@router.post("/login")
+async def login_user(
+    data: UserRequestAdd,
+    response: Response,
+    db: DBDep,
+):
+    user = await db.users.get_user_with_hashed_password(email=data.email)
+    if not user:
+        raise HTTPException(status_code=401, detail="Пользователь с таким email не зарегистрирован")
+    if not AuthService().verify_password(data.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail="Пароль неверный")
+    access_token = AuthService().create_access_token({"user_id": user.id})
+    response.set_cookie("access_token", access_token)
+    return {"access_token": access_token}
+
+
+@router.get("/me")
+async def get_me(user_id: UserIdDep, db: DBDep):
+    user = await db.users.get_one_or_none(id=user_id)
+    return user
+
+
+@router.post("/logout")
+async def logout_user(response: Response):
+    response.delete_cookie('access_token')
+    return {"status": "OK"}
--- app/api/api_v1/dependencies.py
+++ app/api/api_v1/dependencies.py
@@ -1,6 +1,8 @@
 from typing import Annotated
-from fastapi import Depends
+from fastapi import Depends, HTTPException
+from starlette.requests import Request
 
+from services.auth import AuthService
 from utils.db_manager import DBManager
 from core.models import db_helper
 
@@ -11,3 +13,18 @@ async def get_db():
 
 
 DBDep = Annotated[DBManager, Depends(get_db)]
+
+
+def get_token(request: Request) -> str:
+    token = request.cookies.get("access_token", None)
+    if not token:
+        raise HTTPException(status_code=401, detail="Вы не предоставили токен доступа")
+    return token
+
+
+def get_current_user_id(token: str = Depends(get_token)) -> int:
+    data = AuthService().decode_token(token)
+    return data["user_id"]
+
+
+UserIdDep = Annotated[int, Depends(get_current_user_id)]
