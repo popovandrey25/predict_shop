eeaab7ad86a8489a1229cbef071d49c987db1606
andrey <palanelarty@gmail.com>
2024-11-29 14:02:07 +0300

    update and delete items

--- app/api/api_v1/basket.py
+++ app/api/api_v1/basket.py
@@ -24,3 +24,22 @@ async def add_to_basket(
     user_id: UserIdDep,
 ):
     return await db.baskets.add_item_to_basket(user_id, item.product_id, item.quantity)
+
+
+@router.post("/{item_id}")
+async def update_item_in_basket(
+    item_id: int,
+    db: DBDep,
+    user_id: UserIdDep,
+    quantity: int
+):
+    return await db.baskets.update_item_quantity_in_basket(user_id, item_id, quantity)
+
+
+@router.delete("/{item_id}", response_model=BasketDetailResponse)
+async def delete_item_from_basket(
+    item_id: int,
+    db: DBDep,
+    user_id: UserIdDep,
+):
+    return await db.baskets.remove_item_from_basket(user_id, item_id)
--- app/repositories/basket.py
+++ app/repositories/basket.py
@@ -70,3 +70,52 @@ class BasketRepository(BaseRepository):
             for item in basket.items
         ]
         return self.schema(id=basket.id, user_id=basket.user_id, items=basket_items)
+
+
+    async def update_item_quantity_in_basket(self, user_id: int, item_id: int, new_quantity: int):
+        basket = await self._get_basket(user_id)
+        basket_item = next((item for item in basket.items if item.id == item_id), None)
+
+        if not basket_item:
+            raise HTTPException(status_code=404, detail="Item not found in basket")
+
+        basket_item.quantity = new_quantity
+
+        await self.session.commit()
+        await self.session.refresh(basket)
+
+        basket_items = [
+            BasketItemResponse(
+                id=item.id,
+                product_id=item.product_id,
+                quantity=item.quantity,
+                price=item.product.price
+            )
+            for item in basket.items
+        ]
+
+        return self.schema(id=basket.id, user_id=basket.user_id, items=basket_items)
+
+
+    async def remove_item_from_basket(self, user_id: int, item_id: int):
+        basket = await self._get_basket(user_id)
+        basket_item = next((item for item in basket.items if item.id == item_id), None)
+
+        if not basket_item:
+            raise HTTPException(status_code=404, detail="Item not found in basket")
+
+        await self.session.delete(basket_item)
+
+        await self.session.commit()
+        await self.session.refresh(basket)
+
+        basket_items = [
+            BasketItemResponse(
+                id=item.id,
+                product_id=item.product_id,
+                quantity=item.quantity,
+                price=item.product.price
+            )
+            for item in basket.items
+        ]
+        return self.schema(id=basket.id, user_id=basket.user_id, items=basket_items)
